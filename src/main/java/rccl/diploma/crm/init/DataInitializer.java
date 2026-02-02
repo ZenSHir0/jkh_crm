852a076677a70c37a04bcb171e408b52fba28832
ZenSHir0 <pave1kagg@gmail.com>
2026-02-02 15:44:33 +0300

    add admin account on startup

---
+++ src/main/java/rccl/diploma/crm/init/DataInitializer.java
@@ -0,0 +1,30 @@
+package rccl.diploma.crm.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import rccl.diploma.crm.entity.Role;
+import rccl.diploma.crm.entity.User;
+import rccl.diploma.crm.repository.UserRepository;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    public CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User admin = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .email("admin@admin.com")
+                        .fullName("Администратор")
+                        .role(Role.ADMIN)
+                        .enabled(true)
+                        .build();
+                userRepository.save(admin);
+            }
+        };
+    }
+}
