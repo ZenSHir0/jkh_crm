c74bcfc7963e37967430fda5adb0a02c9ba6434f
ZenSHir0 <pave1kagg@gmail.com>
2026-02-03 17:02:30 +0300

    add display names for roles

--- src/main/java/rccl/diploma/crm/entity/Role.java
+++ src/main/java/rccl/diploma/crm/entity/Role.java
@@ -1,5 +1,18 @@
 package rccl.diploma.crm.entity;
 
+@SuppressWarnings("LombokGetterMayBeUsed")
 public enum Role {
-    RESIDENT, MASTER, ADMIN
+    RESIDENT("Житель"),
+    MASTER("Мастер"),
+    ADMIN("Администратор");
+
+    private final String displayName;
+
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
