b7844e7320ceb3e5b7f993de4da9cccc47256243
ZenSHir0 <pave1kagg@gmail.com>
2026-02-09 14:40:29 +0300

    add findByRole to user repository

--- src/main/java/rccl/diploma/crm/repository/UserRepository.java
+++ src/main/java/rccl/diploma/crm/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package rccl.diploma.crm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rccl.diploma.crm.entity.User;
+import rccl.diploma.crm.entity.enums.Role;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByRole(Role role);
+
 }
