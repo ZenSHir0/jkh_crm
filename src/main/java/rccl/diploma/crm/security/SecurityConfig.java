aa885496e124ae41728f734874a9d654637b6913
ZenSHir0 <pave1kagg@gmail.com>
2026-02-20 16:34:55 +0300

    create relative source uploads/

--- src/main/java/rccl/diploma/crm/security/SecurityConfig.java
+++ src/main/java/rccl/diploma/crm/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/verify/**", "/register", "/verify", "/login/**").permitAll()
+                .requestMatchers("/verify/**", "/register", "/verify", "/login/**", "/uploads/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
