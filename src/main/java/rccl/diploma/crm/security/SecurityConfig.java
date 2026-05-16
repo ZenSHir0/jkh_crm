80a52faf1b9326b5c9c28df71f03eef50cbf8ab2
ZenSHir0 <pave1kagg@gmail.com>
2026-05-16 19:10:19 +0300

    styles access fix

--- src/main/java/rccl/diploma/crm/security/SecurityConfig.java
+++ src/main/java/rccl/diploma/crm/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/verify/**", "/register", "/verify", "/login/**", "/uploads/**").permitAll()
+                .requestMatchers("/verify/**", "/register", "/verify", "/login/**", "/uploads/**", "/css/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
