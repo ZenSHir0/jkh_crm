37e981f07c0ae5a8e3d1a96800d2429d2e4b3e4f
ZenSHir0 <pave1kagg@gmail.com>
2026-05-22 11:34:36 +0300

    navbar changed

--- src/main/java/rccl/diploma/crm/config/MvcConfig.java
+++ src/main/java/rccl/diploma/crm/config/MvcConfig.java
@@ -1,6 +1,12 @@
 package rccl.diploma.crm.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +18,20 @@ public class MvcConfig implements WebMvcConfigurer {
         String uploadDir = System.getenv("UPLOADS_DIR");
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(@NonNull HttpServletRequest request,
+                                   @NonNull HttpServletResponse response,
+                                   @NonNull Object handler,
+                                   ModelAndView modelAndView) {
+                if (modelAndView != null) {
+                    modelAndView.addObject("currentUri", request.getRequestURI());
+                }
+            }
+        });
     }
 }
